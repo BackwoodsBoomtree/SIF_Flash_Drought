19b3123d339f773b63baead5960571e2f7adaa82
Doughty <russell.doughty@ou.edu>
2021-10-19 18:06:26 -0500

    Extract data by fd event

--- annual_fd_maps.R
+++ annual_fd_maps.R
@@ -4,7 +4,7 @@ library(RColorBrewer)
 
 
 fd_dir         <- "G:/flash_drought/fix_extent" # Main directory
-sub_dir_list   <- list.dirs(path = fd_dir, full.names = TRUE, recursive = FALSE) # Subdirectories for each year
+sub_dir_list   <- list.dirs(path = fd_dir, full.names = TRUE, recursive = FALSE) # Subdirs for each year
 out_dir_n      <- "G:/flash_drought/analysis/n"
 out_dir_start  <- "G:/flash_drought/analysis/start"
 out_dir_length <- "G:/flash_drought/analysis/length"
@@ -43,7 +43,7 @@ for (sub_dir in sub_dir_list) {
   
   # Sum up the raster stack into a single raster
   annual_n <- sum(annual_n, na.rm = TRUE)
-  plot(annual_n, main = paste0("Number of Flash Droughts ", year), col = plasma(256))
+  plot(annual_n, main = paste0("Number of Flash Droughts ", year), col = plasma(256), ylim = c(25, 50))
   writeRaster(annual_n, paste0(out_dir_n, "/n_flash_droughts_", year, ".tif"), overwrite = TRUE, NAflag = -9999)
   
   #### Create annual maps of start date and length ####
@@ -78,8 +78,8 @@ for (sub_dir in sub_dir_list) {
   annual_length <- sum(annual_length, na.rm = TRUE)
   
   # Plot and save
-  plot(annual_start, main = paste0("Start of Flash Drought (DOY) for Gridcells with One Flash Drought ", year), col = brewer.pal(11, "RdYlBu"))
-  plot(annual_length, main = paste0("Length of Flash Drought (pentads) for Gridcells with One Flash Drought ", year), col = viridis(7))
+  plot(annual_start, main = paste0("Start of Flash Drought (DOY) for Gridcells with One Flash Drought ", year), col = brewer.pal(11, "RdYlBu"), ylim = c(25, 50))
+  plot(annual_length, main = paste0("Length of Flash Drought (pentads) for Gridcells with One Flash Drought ", year), col = viridis(7), ylim = c(25, 50))
   
   writeRaster(annual_start, paste0(out_dir_start, "/doy_flash_droughts_", year, ".tif"), overwrite = TRUE, NAflag = -9999)
   writeRaster(annual_length, paste0(out_dir_length, "/length_flash_droughts_", year, ".tif"), overwrite = TRUE, NAflag = -9999)
---
+++ fd_event_2019_03_Great_Plains.R
@@ -0,0 +1,81 @@
+library(terra)
+library(viridis)
+library(RColorBrewer)
+
+# Import map of start dates for masking SIF and VI data
+doy_tif         <- rast("G:/flash_drought/analysis/start/doy_flash_droughts_2019.tif") # Main directory
+sif_esa_735     <- sds("G:/TROPOMI/esa/gridded/TROPOMI.SIF.201805-202108.0125deg.modisLike.esa.735nm.nc")
+sif_esa_743     <- sds("G:/TROPOMI/esa/gridded/TROPOMI.SIF.201805-202108.0125deg.modisLike.esa.743nm.nc")
+year            <- 2019
+doy_filters     <- c(61, 66, 71, 76)    # DOYs to be used as a fd event
+fd_event_coords <- c(-105, -91, 38, 43) # xmin, xmax, ymin, ymax for fd event
+
+fd_event_mask   <- function(doy_tif, year, doy_filters, fd_event_coords) {
+  # Create a 2 column matrix of is and becomes for creating mask
+  m <- matrix(NA, nrow = 0, ncol = 2)
+  for (i in seq.Date(as.Date(paste0(year, "/1/1")), as.Date(paste0(year, "/12/31")), by = "5 days")) {
+    doy <- as.numeric(strftime(as.Date(i, origin = "1970-01-01"), format = "%j"))
+  
+    if (doy %in% doy_filters) {
+      m <- rbind(m, c(doy, 1))
+      # df[nrow(df) + 1,] = c(doy, 1)
+    } else {
+      m <- rbind(m, c(doy, NA))
+      # df[nrow(df) + 1,] = c(doy, NA)
+    }
+  }
+  
+  # Crop mask to fd event, then extend extent to match other data
+  doy_tif <- classify(doy_tif, m)
+  doy_tif <- crop(doy_tif, fd_event_coords)
+  doy_tif <- extend(doy_tif, ext(sif_esa_743))
+  plot(doy_tif, main = paste0("Flash Drought Area ", year))
+  
+  return(doy_tif)
+}
+
+data_means      <- function(input, fd_mask) {
+ 
+  df <- data.frame(matrix(NA, ncol = length(names(input))))
+  
+  for (i in 1:length(nlyr(input))) {
+    
+    colnames(df[i]) <- names(input)[i]
+    
+    for (j in 1:nlyr(input)[i]) {
+      df[j, i] <- as.numeric(global(mask(input[i][[j]], fd_mask, inverse = TRUE, maskvalue = 1, updatevalue = NA), fun = mean, na.rm = TRUE))
+    }
+  }
+  
+  colnames(df) <- names(input)
+
+  # Take a look! (max n of plots is 10, so generate more plots as needed)
+  if (ncol(df) <= 10) {
+    plot.ts(df, main = "Time Series of Data for Flash Drought Area")
+  } else {
+    for (i in 1:ceiling(ncol(df) / 10)) {
+      last_pos <- i * 10
+      print(i)
+      print("Last Position i * 10")
+      print(last_pos)
+      if (last_pos > ncol(df)) {
+        last_pos <- ncol(df)
+        print(last_pos)
+        print("Last Position ncol(df_735)")
+      }
+      plot.ts(df[, c((i * 10 - 9):last_pos)], main = "Time Series of Data for Flash Drought Area")
+      print((i * 10 - 9):last_pos)
+      print("Last Position last")
+    }
+  }
+  return(df)
+}
+
+
+fd_mask <- fd_event_mask(doy_tif, year, doy_filters, fd_event_coords)
+
+df_743  <- data_means(sif_esa_743, fd_mask)
+df_735  <- data_means(sif_esa_735, fd_mask)
+
+
+
